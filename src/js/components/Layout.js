659bddf47e41fc0370ce876d1d8d77d0d322e540
Thomas Babu <joseph.thomas.babu@gmail.com>
2016-06-25 17:27:17 +0800

    Composing Multiple React.js Components

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,21 +1,17 @@
 import React from "react"
 
-export default class Layout extends React.Component {
-	constructor() {
-		super();
-		this.name = "Thomas";
-	}
+import Footer from "./Footer.js"
+import Header from "./Header.js"
 
-	calculateVal(a,b) {
-		return a + b;
-	}
+export default class Layout extends React.Component {
+
 	render() {
 		return (
 			<div>
-				<h1>It {this.name}!!</h1>
-				<h2>4 + 3 = {this.calculateVal(4,3)}</h2>
-				<h3>how is it ?</h3>
+				<Header />
+				<Footer />
 			</div>
-		);
+			
+		)
 	}
 }
